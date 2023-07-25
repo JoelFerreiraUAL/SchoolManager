9720187d92c86a0c60e2a51b1895beea48cca827
Joel Ferreira <30007769@students.ual.pt>
2023-07-26 00:00:15 +0100

    ZDSC

--- src/SchoolManager.BackOfficeWebUI/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace SchoolManager.BackOfficeWebUI.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
